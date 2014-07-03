1da6e16f0c51a90c8c557d517aa289ffde2f7f5c
Zi Shen Lim <zlim.lnx@gmail.com>
2014-07-03 07:56:54 -0700

    net: filter: move load_pointer() into filter.h
    load_pointer() is already a static inline function.
    Let's move it into filter.h so BPF JIT implementations can reuse this
    function.
    
    Since we're exporting this function, let's also rename it to
    bpf_load_pointer() for clarity.
    
    Signed-off-by: Zi Shen Lim <zlim.lnx@gmail.com>
    Reviewed-by: Daniel Borkmann <dborkman@redhat.com>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I7caaaff8b17f5e979a89697e76a19ef930450b9e

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -11,6 +11,7 @@
 #ifdef __KERNEL__
 #include <linux/atomic.h>
 #include <linux/compat.h>
+#include <linux/skbuff.h>
 #include <linux/workqueue.h>
 #endif
 
@@ -540,6 +541,18 @@ static inline u16 bpf_anc_helper(const struct sock_filter *ftest)
 	default:
 		return ftest->code;
 	}
+}
+
+void *bpf_internal_load_pointer_neg_helper(const struct sk_buff *skb,
+					   int k, unsigned int size);
+
+static inline void *bpf_load_pointer(const struct sk_buff *skb, int k,
+				     unsigned int size, void *buffer)
+{
+	if (k >= 0)
+		return skb_header_pointer(skb, k, size, buffer);
+
+	return bpf_internal_load_pointer_neg_helper(skb, k, size);
 }
 
 #ifdef CONFIG_BPF_JIT
