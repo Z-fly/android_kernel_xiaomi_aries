bbbbf2c98efbada4b9f239a11289550734391eb7
GuaiYiHu <guaiyihu@foxmail.com>
2026-08-17 09:02:44 +0800

    mm: backport MADV_WIPEONFORK

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -127,6 +127,7 @@ extern unsigned int kobjsize(const void *objp);
 #endif
 #define VM_INSERTPAGE	0x02000000	/* The vma has had "vm_insert_page()" done on it */
 #define VM_NODUMP	0x04000000	/* Do not include in the core dump */
+#define VM_WIPEONFORK	0x08000000	/* Wipe VMA contents in child */
 
 #define VM_MIXEDMAP	0x10000000	/* Can contain "struct page" and pure PFN pages */
 #define VM_SAO		0x20000000	/* Strong Access Ordering (powerpc) */
